cb8b9b1391d6b43ad4c5770e669f4db4a267aecb
Davidfhk <davidign1688@hotmail.com>
2017-07-02 18:17:36 +0200

    Creación de nuestra template de post "index.php". Aqui mostraremos los posts y el formulario para añadir post

---
+++ app/templates/post/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Blog</title>
+<!-- 	<link rel="stylesheet" type="text/css" href="../public/css/style.css"> -->
+</head>
+<body>
+
+		<!-- recorremos el array de objetos -->
+		
+			<?php foreach($posts as $post): ?>
+				<h3><?= $post->getTitle() ?></h3>
+				<h4><?= $post->getDatePost() ?></h4>
+				<div style='width:400px'>
+					<?= $post->getComment() ?>
+				</div>
+				<img src="../public/image/<?= $post->getImage()?>" style='width:100px' />
+				<hr>
+			<?php endforeach ?>
+
+		
+	<table>
+		<form action="<?='addPost/'?>" method="POST">
+			<tr>
+				<td></td>
+			</tr>
+			<tr>
+				<td><input type="text" name="title"></td>
+			</tr>
+			<tr>
+				<td><textarea name="comment"></textarea></td>
+			</tr>
+			<input type="hidden" name="MAX_TAM" value="2097152">
+			<tr>
+				<td><input type="submit" name="enviar" value="Insertar"></td>
+			</tr>
+		</form>
+	</table>
+</body>
+</html>
